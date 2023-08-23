4e671b6d3ec9da1a599d1dcceab5c544b518359a
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-08-22 19:05:11 -0700

    database created

---
+++ routes/tasks.js
@@ -0,0 +1,21 @@
+const knex = require('knex')(require('../knexfile'));
+const express = require('express')
+const router = express.Router();
+const {v4: uuid} = require('uuid');
+
+
+router.get('/', (req, res)=>{
+
+})
+
+router.get('/:id',(req, res)=>{
+
+})
+
+router.put('/:id', (req,res)=>{
+    
+})
+
+router.post('/',(req, res)=>{
+
+})
