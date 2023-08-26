ca1b592094bd7bbb815da49f6bcf063d5fbb15e6
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-08-26 10:38:04 -0700

    Password hashed

--- index.js
+++ index.js
@@ -7,18 +7,23 @@ require('dotenv').config();
 const cors = require('cors');
 
 const {CORS_ORIGIN} = process.env;
-app.use(cors({origin: CORS_ORIGIN}));
+
 
 const PORT = process.env;
 
 const taskRoutes = require('./routes/tasks');
 const userRoutes = require('./routes/user');
+
+// middleware
+app.use(cors({origin: CORS_ORIGIN}));
+app.use(express.static('public'));
 app.use(express.json());
 app.use('/task',taskRoutes);
 app.use('/user', userRoutes);
-
 app.use(express.json());
 
+
+//routes
 app.get('/',(req, res) => {
     res.send('<h1>Hello</h1>')
 }
--- routes/user.js
+++ routes/user.js
@@ -2,21 +2,28 @@ const knex = require('knex')(require('../knexfile'));
 const express = require('express');
 const router = express.Router();
 const {v4:uuid} = require('uuid');
+const bcrypt = require('bcrypt');
 
 router.post('/sign-up',(req, res)=>{
-    const newUser = {
-        id: uuid(),
-        f_name: req.body.f_name,
-        l_name: req.body.l_name,
-        email: req.body.email,
-        password: req.body.password
-    }
-    console.log(newUser)
-    knex('user').insert(newUser).then((newUser)=>{
-        return res.status(201).json(newUser)
-    }).catch((e)=>{
-        console.log(e)
+
+    const plainPassword = req.body.password;
+
+    bcrypt.hash(plainPassword, 20).then(hash=>{
+        const newUser = {
+            id: uuid(),
+            f_name: req.body.f_name,
+            l_name: req.body.l_name,
+            email: req.body.email,
+            password: hash
+        }
+        console.log(newUser)
+        knex('user').insert(newUser).then((newUser)=>{
+            return res.status(201).json(newUser)
+        }).catch((e)=>{
+            console.log(e)
+        })
     })
+    
 })
 
 module.exports = router;
