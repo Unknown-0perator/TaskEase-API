81ff3939def80b6b210b9a0f30d6a2071f57bd09
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-09-02 20:25:19 -0700

    seeding data added to tables

--- migrations/20230822234234_create_users_tables.js
+++ migrations/20230822234234_create_users_tables.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
     return knex.schema 
         .createTable('users', (table)=>{
             table.uuid('user_id').primary();
-            table.string('fist_name').notNullable();
+            table.string('first_name').notNullable();
             table.string('last_name').notNullable();
             table.string('email').notNullable();
             table.string('password').notNullable();
