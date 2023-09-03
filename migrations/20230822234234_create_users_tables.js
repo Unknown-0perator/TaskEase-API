de2d193ba98aa737af939bc5df826fc5a2ca2b8c
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-09-02 19:07:40 -0700

    tables created

---
+++ migrations/20230822234234_create_users_tables.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+    return knex.schema 
+        .createTable('users', (table)=>{
+            table.uuid('user_id').primary();
+            table.string('fist_name').notNullable();
+            table.string('last_name').notNullable();
+            table.string('email').notNullable();
+            table.string('password').notNullable();
+            table.timestamp('created_at').defaultTo(knex.fn.now());
+        })
+}
+
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('users');
+}
---
+++ migrations/20230903013821_create_comments_table.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+    return knex.schema 
+        .createTable('comments', (table) => {
+            table.uuid('comment_id').primary();
+            table.string('comment_text').notNullable();
+            table.timestamp('timestamp').defaultTo(knex.fn.now());
+            table.uuid('task_id').references('tasks.task_id').onUpdate('CASCADE')
+            .onDelete('CASCADE').notNullable();
+            table.uuid('user_id').references('users.user_id').onUpdate('CASCADE')
+            .onDelete('CASCADE').notNullable();
+        })
+}
+
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('comments');
+}
