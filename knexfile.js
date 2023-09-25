facf3108dbf381be10ab3cc85265ec44f80e7bba
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-09-25 16:50:54 -0700

    refactoring

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   connection: {
     host: '127.0.0.1',
     database: process.env.DB_LOCAL_DNAME,
-    user:process.env.DB_LOCAL_USER,
-    password:process.env.DB_LOCAL_PASSWORD,
+    user: process.env.DB_LOCAL_USER,
+    password: process.env.DB_LOCAL_PASSWORD,
   }
 }
